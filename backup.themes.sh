1c4320d5165b1836aeff8d7365df1fd32e2156bf
Viet Le <lecaoviet@gmail.com>
2024-08-13 14:24:49 -0400

    Update themes script

--- backup.themes.sh
+++ backup.themes.sh
@@ -2,9 +2,9 @@ echo "production environment"
 pod=$(kubectl get pods -n main | grep Running | grep 'onelink' | awk '{print $1}')
 
 echo "Backup users's themes"
-kubectl cp $pod:/htdocs/themes ./linkstack/themes -n main
+kubectl cp $pod:/htdocs/themes ./linkstack/themes -c onelink -n main
 
 echo "Backup users's avatars"
-kubectl cp $pod:/htdocs/assets/img ./linkstack/assets/img -n main
+kubectl cp $pod:/htdocs/assets/img ./linkstack/assets/img -c onelink -n main
 
 echo "Done"
