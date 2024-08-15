90ed3313d9a42c002a61255139755008c89aa766
Viet Le <lecaoviet@gmail.com>
2024-08-15 16:55:31 -0400

    Debuging...

--- linkstack/resources/views/home.blade.php
+++ linkstack/resources/views/home.blade.php
@@ -7,8 +7,8 @@
     <meta charset="utf-8">
     @php $GLOBALS['themeName'] = config('advanced-config.home_theme'); @endphp
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-      <meta http-equiv="Content-Security-Policy-Report-Only" content="frame-ancestors 'self'">
+      <!--meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+      <meta http-equiv="Content-Security-Policy" content="frame-ancestors *"-->
       @if(env('CUSTOM_META_TAGS') == 'true' and config('advanced-config.title') != '')
       <title>{{ config('advanced-config.title') }}</title>
       @else
