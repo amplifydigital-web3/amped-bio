7fefef65b1e172a7bf32684516cd1be2a3967ec1
Viet Le <lecaoviet@gmail.com>
2024-08-19 08:42:18 -0400

    Debugging - CSP frame ancestor

--- linkstack/resources/views/linkinfo.blade.php
+++ linkstack/resources/views/linkinfo.blade.php
@@ -8,6 +8,7 @@
     @php $GLOBALS['themeName'] = config('advanced-config.home_theme'); @endphp
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <!--meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" -->
+      <meta http-equiv="Content-Security-Policy" content="frame 'self' https://secure.walletconnect.org https://verify.walletconnect.org/">  
       @if(env('CUSTOM_META_TAGS') == 'true' and config('advanced-config.title') != '')
       <title>{{ config('advanced-config.title') }}</title>
       @else
