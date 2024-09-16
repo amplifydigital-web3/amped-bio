6cc439ff4872c1d965190f415982e0d8eab802c3
Gustavo <gustavo@obex.pw>
2024-09-16 16:48:20 +0100

    created docker compose file to handle dev environment

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,8 +1,10 @@
 FROM php:8.1
 
-RUN apt-get update -y
-RUN apt-get install -y unzip libpq-dev libcurl4-gnutls-dev
-RUN docker-php-ext-install pdo pdo_mysql bcmath ext-zip ext-gd
+RUN apt-get update -y && \
+    apt-get install -y unzip libpq-dev libcurl4-gnutls-dev libpng-dev libjpeg-dev libfreetype6-dev libzip-dev
+
+RUN docker-php-ext-configure gd --with-freetype --with-jpeg && \
+    docker-php-ext-install pdo pdo_mysql bcmath zip gd
 
 RUN pecl install -o -f redis \
     && rm -rf /tmp/pear \
--- docker/entrypoint.dev.sh
+++ docker/entrypoint.dev.sh
@@ -11,7 +11,7 @@ else
     echo "env file exist."
 fi
 
-php artisan migrate
+# php artisan migrate
 php artisan key:generate
 php artisan cache:clear
 php artisan config:clear
