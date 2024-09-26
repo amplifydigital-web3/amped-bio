7c606cf22cddf3ebb41262f6770c7ab6eb964b84
Ed McNierney <ed@mcnierney.com>
2024-09-26 16:51:37 -0400

    Add Plausible analytics

--- linkstack/resources/views/layouts/analytics.blade.php
+++ linkstack/resources/views/layouts/analytics.blade.php
@@ -12,7 +12,11 @@ $analyticsHTML = trim($analyticsHTML);
 <!-- /Analytics -->
 @endif
 
-<!-- Google tag (gtag.js) --> 
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-RJJX8Q9P3R"></script>
 <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-RJJX8Q9P3R'); </script>
 <!-- End of Google tag (gtag.js) --> 
+
+<!-- Plausible analytics -->
+<script defer data-domain="onelink.npayme.io" src="https://plausible.io/js/script.js"></script>
+<!-- End of Plausible analytics -->
