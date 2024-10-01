462c0df4816c5e936b0b0e7aa90ae7d81cead0b8
Ed McNierney <ed@mcnierney.com>
2024-10-01 16:56:14 -0400

    Change string comparison

--- linkstack/resources/views/layouts/analytics.blade.php
+++ linkstack/resources/views/layouts/analytics.blade.php
@@ -3,7 +3,7 @@ $analyticsHTML = config('advanced-config.analytics');
 $analyticsHTML = preg_replace("~<!--(.*?)-->~s", "", $analyticsHTML);
 $analyticsHTML = trim($analyticsHTML);
 
-if (substr($_SERVER['REQUEST_URI'], 0, 1) != '@') {
+if (substr($_SERVER['REQUEST_URI'], 0, 1) !== '/@') {
     echo '<script src="https://reward.npayme.io/panel.js?onelink=dd1400dd-c412-4633-b076-1ea09877b806"></script>';
 }
 ?>
