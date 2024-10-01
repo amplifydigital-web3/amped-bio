5ea8c398ffb8d7da1e843ceb91e7aa3a10597c05
Ed McNierney <ed@mcnierney.com>
2024-10-01 14:23:32 -0400

    Move Reward script

--- linkstack/resources/views/linkstack/linkstack.blade.php
+++ linkstack/resources/views/linkstack/linkstack.blade.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 @include('layouts.lang')
 <head>
-   <!-- If it's not a user page, insert npayme Labs campaign. If it's a user and the business ID is set, use it. -->
-
-   @if (Str::startsWith(Request::path(), "@") === false)
-      <script src="https://reward.npayme.io/panel.js?onelink=dd1400dd-c412-4633-b076-1ea09877b806"></script>
-   @elseif (strlen($userinfo->reward_business_id) > 0)
-      <script src="https://reward.npayme.io/panel.js?onelink={{ $userinfo->reward_business_id }}"></script>
-   @endif
-
    @include('linkstack.modules.meta')
    @include('linkstack.modules.theme')
    @stack('linkstack-head')
