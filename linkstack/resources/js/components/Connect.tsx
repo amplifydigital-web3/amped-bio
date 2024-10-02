800b8677f277837b3a36e7cdee7b5f0c36fa0f1b
Gustavo <gustavo@obex.pw>
2024-10-02 15:11:09 +0100

    return old code

--- linkstack/resources/js/components/Connect.tsx
+++ linkstack/resources/js/components/Connect.tsx
@@ -1,11 +1,19 @@
 import { useContext } from "react";
+import { useAccount } from "wagmi";
 
 import { AppContext } from ".";
 
 export default function Web3ConnectButton() {
   const ctx = useContext<any>(AppContext);
-  const { address, openWeb3Modal, openModal } = ctx;
+  const { address, setAddress, openWeb3Modal, openModal } = ctx;
+  const { address: newAddress, isConnecting, isReconnecting } = useAccount();
 
+  console.log("newAddress................. 1", newAddress);
+  if (newAddress && newAddress != address) {
+    console.log("update newAddress................. 1", newAddress);
+    setAddress(newAddress);
+  }
+  
   const handleClick = () => {
     if (address) {
       openWeb3Modal();
