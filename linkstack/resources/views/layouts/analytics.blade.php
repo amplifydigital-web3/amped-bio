621d1af48f23d3ee1866ca9f707a46bd38cf2c79
Viet Le <lecaoviet@gmail.com>
2024-11-27 16:28:05 -0500

    Apply REWARD_BUTTON_URL env. var

--- linkstack/resources/views/layouts/analytics.blade.php
+++ linkstack/resources/views/layouts/analytics.blade.php
@@ -2,9 +2,10 @@
 $analyticsHTML = config('advanced-config.analytics');
 $analyticsHTML = preg_replace("~<!--(.*?)-->~s", "", $analyticsHTML);
 $analyticsHTML = trim($analyticsHTML);
+$rewardButtonURL = env('REWARD_BUTTON_URL');
 
 if (substr($_SERVER['REQUEST_URI'], 0, 2) !== '/@') {
-    echo '<script src="https://reward.npayme.io/panel.js?onelink=dd1400dd-c412-4633-b076-1ea09877b806"></script>';
+    echo "<script src=\"{$rewardButtonURL}?onelink=dd1400dd-c412-4633-b076-1ea09877b806\"></script>";
 } 
 ?>
 
