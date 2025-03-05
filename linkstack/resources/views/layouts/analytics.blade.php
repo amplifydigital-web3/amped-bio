d22f55466fbd290acd024e02817ea76c87844544
Ed McNierney <ed@mcnierney.com>
2025-03-05 14:33:00 -0500

    Move help button removal

--- linkstack/resources/views/layouts/analytics.blade.php
+++ linkstack/resources/views/layouts/analytics.blade.php
@@ -6,6 +6,9 @@ $rewardButtonURL = env('REWARD_BUTTON_URL');
 
 if (substr($_SERVER['REQUEST_URI'], 0, 2) !== '/@') {
     echo "<script src=\"{$rewardButtonURL}?onelink=dd1400dd-c412-4633-b076-1ea09877b806\"></script>";
+    echo "<script>window.fwSettings={'widget_id':154000003485};";
+	echo "!function(){if(\"function\"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()</script>";
+    echo "<script type='text/javascript' src='https://widget.freshworks.com/widgets/154000003485.js' async defer></script>";
 } 
 ?>
 
