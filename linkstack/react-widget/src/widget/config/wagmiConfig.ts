732488a21265f4c8ed6ef4b07b260c24a736ad5a
Gustavo <gustavo@obex.pw>
2025-03-17 19:58:25 -0300

    add coinbaseWallet to conenctors array

--- linkstack/react-widget/src/widget/config/wagmiConfig.ts
+++ linkstack/react-widget/src/widget/config/wagmiConfig.ts
@@ -3,6 +3,7 @@ import {
   mainnet,
   AppKitNetwork,
 } from '@reown/appkit/networks';
+import { coinbaseWallet } from 'wagmi/connectors'
 
 export const projectId = "e0c0b073645ed7ce3b15930fd2847d6d";
 
@@ -18,4 +19,7 @@ export const wagmiAdapter = new WagmiAdapter({
   ssr: false,
   projectId,
   networks,
+  connectors: [
+    coinbaseWallet()
+  ]
 });
